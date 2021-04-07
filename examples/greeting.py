89657e792de1d900d24264aab2efd0798a7748bf
Johannes Rieke <johannes.rieke@gmail.com>
2021-04-07 02:40:59 +0200

    Add initial structure

---
+++ examples/greeting.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def method_2():
+    st.write("Hello World")
+
+def method1():
+    name = st.text_input("Enter your name")
+    st.write("Hello", name)
+
+
---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+"Hello"
+
+with st.sidebar:
+    st.radio("Examples", ["Overview", "Greeting (by Johannes)"])
+    st.radio("Method", ["World", "Custom name"])
